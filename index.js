cf6813136b41c978d9390df6582b6eeb958a7e53
mashuq0068 <mashuq0068@gmail.com>
2023-11-17 00:20:20 +0600

    added patch user

--- index.js
+++ index.js
@@ -69,6 +69,26 @@ async function run() {
     const result = await cursor.toArray()
     res.send(result)
 })
+app.delete('/user/:id' , async(req, res) => {
+  const id = req.params.id
+  const query = {_id : new ObjectId(id)}
+  const result  =await userCollection.deleteOne(query)
+  res.send(result)
+})
+app.patch('/user/admin/:id' , async(req, res) => {
+  const id = req.params.id
+  const filter = {_id : new ObjectId(id)}
+  const options = { upsert: true };
+
+  const updatedUser  = {
+    $set : {
+      role:'admin'
+    }
+  }
+  const result = await userCollection.updateOne(filter, updatedUser, options);
+    
+  res.send(result)
+})
    
    
    
