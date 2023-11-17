9247f10b2e558f659bb539fa668232e74f31c762
mashuq0068 <mashuq0068@gmail.com>
2023-11-18 01:30:33 +0600

    added menu post

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app  =express()
 const port = process.env.PORT || 5000
 require ('dotenv').config()
 const cors = require("cors")
+const jwt = require("jsonwebtoken")
 app.use(cors())
 app.use(express.json())
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -15,6 +16,21 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+const verifyToken = (req , res ,next) => {
+  console.log(req.headers.authorization)
+  if(!req.headers.authorization){
+  return res.status(401).send({message : "Unauthorized Access"})
+  }
+ const token = req.headers.authorization.split(' ')[1]
+ jwt.verify(token , process.env.TOKEN_SECRET , (err , decoded) => {
+  if(err){
+    return res.status(401).send({message : "Unauthorized Access"})
+  }
+  req.decoded = decoded;
+  next();
+ })
+
+}
 
 async function run() {
   try {
@@ -28,6 +44,11 @@ async function run() {
         const cursor = menuCollection.find()
         const result =await cursor.toArray()
         res.send(result)
+    })
+    app.post('/menu' , async(req ,res) => {
+      const menu  = req.body
+      const result =await menuCollection.insertOne(menu)
+      res.send(result)
     })
     app.get('/reviews' , async(req ,res) => {
         const cursor = reviewCollection.find()
@@ -64,11 +85,26 @@ async function run() {
     const result = await userCollection.insertOne(newUser)
  return res.send(result)
   })
-  app.get('/users' , async(req ,res) => {
+  app.get('/users',verifyToken, async(req ,res) => {
     const cursor = userCollection.find()
     const result = await cursor.toArray()
     res.send(result)
 })
+app.get('/user/admin/:email' ,verifyToken, async(req, res) => {
+   const email = req.params.email
+   if(email !== req.decoded?.email){
+     return res.status(403).send("forbidden")
+   }
+   const query = {email : email}
+   const user = await userCollection.findOne(query)
+   let admin = false
+   if(user?.role){
+    admin = true
+   }
+   res.send({admin})
+})
+
+
 app.delete('/user/:id' , async(req, res) => {
   const id = req.params.id
   const query = {_id : new ObjectId(id)}
@@ -88,6 +124,11 @@ app.patch('/user/admin/:id' , async(req, res) => {
   const result = await userCollection.updateOne(filter, updatedUser, options);
     
   res.send(result)
+})
+app.post('/jwt' , async(req , res) => {
+  const user = req.body
+  const token = jwt.sign(user , process.env.TOKEN_SECRET)
+  res.send({token})
 })
    
    
